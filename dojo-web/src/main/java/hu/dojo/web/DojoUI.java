ebf13f48958de7daeb8eaf0a3ad9b774950ff508
Gergő Dulai <dulaig93@gmail.com>
2018-03-12 12:08:45 +0100

    Fixed dojo-web module.

--- dojo-web/src/main/java/hu/dojo/web/DojoUI.java
+++ dojo-web/src/main/java/hu/dojo/web/DojoUI.java
@@ -1,15 +1,17 @@
-package com.example.dojoweb;
+package hu.dojo.web;
 
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.Label;
 import com.vaadin.annotations.Theme;
+import com.vaadin.cdi.CDIUI;
 
 @Theme("DojoWeb")
-public class DojoWebUI extends UI{
-	
+@CDIUI("")
+public class DojoUI extends UI {
+
 	@Override
-	protected void init(VaadinRequest request){
+	protected void init(VaadinRequest request) {
 		Label lbl = new Label("Hello vaadin");
 		setContent(lbl);
 	}
