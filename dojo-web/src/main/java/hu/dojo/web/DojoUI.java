3efbba89e82eef17b67aa5cad7dd91231766d94a
Gergő Dulai <dulaig93@gmail.com>
2018-03-12 12:15:12 +0100

    Fixed dojo-backend module

--- dojo-web/src/main/java/hu/dojo/web/DojoUI.java
+++ dojo-web/src/main/java/hu/dojo/web/DojoUI.java
@@ -1,14 +1,22 @@
 package hu.dojo.web;
 
-import com.vaadin.server.VaadinRequest;
-import com.vaadin.ui.UI;
-import com.vaadin.ui.Label;
+import javax.ejb.EJB;
+
 import com.vaadin.annotations.Theme;
 import com.vaadin.cdi.CDIUI;
+import com.vaadin.server.VaadinRequest;
+import com.vaadin.ui.Label;
+import com.vaadin.ui.UI;
+
+import hu.dojo.backend.IEntityDAO;
+import hu.dojo.jpa.UserAccount;
 
 @Theme("DojoWeb")
 @CDIUI("")
 public class DojoUI extends UI {
+
+	@EJB(beanName = "UserAccountDAO")
+	private IEntityDAO<UserAccount> dao;
 
 	@Override
 	protected void init(VaadinRequest request) {
