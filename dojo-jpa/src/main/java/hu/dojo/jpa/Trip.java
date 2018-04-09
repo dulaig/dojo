3030a276d2a4628dfeda4d86b4a75fe47d40c099
Gergő Dulai <dulaig93@gmail.com>
2018-04-09 14:24:58 +0200

    Added JPA class to persistence.xml

--- dojo-jpa/src/main/java/hu/dojo/jpa/Trip.java
+++ dojo-jpa/src/main/java/hu/dojo/jpa/Trip.java
@@ -8,13 +8,13 @@ import javax.persistence.Table;
 
 @Entity
 @Table
-public class Trip extends AbstractEntity{
+public class Trip extends AbstractEntity {
 
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	
+
 	@Column
 	private String from;
 	@Column
@@ -23,31 +23,37 @@ public class Trip extends AbstractEntity{
 	private LocalDateTime departure;
 	@Column
 	private LocalDateTime arrival;
-	
+
 	public String getFrom() {
 		return from;
 	}
+
 	public void setFrom(String from) {
 		this.from = from;
 	}
+
 	public String getTo() {
 		return to;
 	}
+
 	public void setTo(String to) {
 		this.to = to;
 	}
+
 	public LocalDateTime getDeparture() {
 		return departure;
 	}
+
 	public void setDeparture(LocalDateTime departure) {
 		this.departure = departure;
 	}
+
 	public LocalDateTime getArrival() {
 		return arrival;
 	}
+
 	public void setArrival(LocalDateTime arrival) {
 		this.arrival = arrival;
 	}
-	
-	
+
 }
