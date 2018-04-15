b47b49d5de713fabbe997e022bd757945a792617
Bálint Bogáti <balint1994@gmail.com>
2018-04-15 21:30:35 +0200

    Session létrehozva

--- dojo-web/src/main/java/hu/dojo/web/DojoUI.java
+++ dojo-web/src/main/java/hu/dojo/web/DojoUI.java
@@ -9,6 +9,8 @@ import com.vaadin.navigator.Navigator;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.ui.UI;
 
+import hu.dojo.jpa.SessionData;
+
 @Theme("DojoWeb")
 @CDIUI("")
 public class DojoUI extends UI {
@@ -19,8 +21,11 @@ public class DojoUI extends UI {
 	@Inject
 	private MainLayout mainLayout;
 	
+	public static SessionData sessionData;
+	
 	@Override
-	protected void init(VaadinRequest request) {		
+	protected void init(VaadinRequest request) {
+		sessionData = SessionData.getInstance();
 		Navigator navigator = new Navigator(this, mainLayout.getContainer());
 		navigator.addProvider(viewProvider);
 		navigator.navigateTo("index");
